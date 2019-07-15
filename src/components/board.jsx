a063155986e9f306c737c2d56d2fdf054d6cfa51
Heather Flaten <hflatn@gmail.com>
2019-07-15 15:11:25 -0600

    First Commit

---
+++ src/components/board.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import boards from './board.css';
+
+
+class board extends Component {
+    state = {  }
+    render() { 
+        return (
+             <React.Component>
+
+                <div className = "outline">
+
+                </div>
+
+
+        </React.Component> 
+        );
+    }
+}
+ 
+export default board;
---
+++ src/components/menu.jsx
@@ -0,0 +1,19 @@
+import React, {components} from 'React';
+
+class menu extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <React.Fragment>
+
+
+
+
+
+
+        </React.Fragment> 
+        );
+    }
+}
+ 
+export default menu;
